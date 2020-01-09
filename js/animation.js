c9cd3708d46f4aee7e1ad6955a5aa49b3b80b956
Elber <elbercastillomesa@gmail.com>
2020-01-09 00:22:49 -0500

    fixed a bug where the background doesn't change using the slider bar

--- js/animation.js
+++ js/animation.js
@@ -8,17 +8,21 @@ document.getElementById('less').addEventListener("click", function(event){
 	sliderOutput("output", document.getElementById('range').value);
 });
 
+document.getElementById('range').addEventListener("change", function(event){	
+	sliderOutput("output", document.getElementById('range').value);
+});
+
+
 function sliderOutput(variable, value){
     var s = document.getElementById(variable);
     s.value = value;
     
     if (s.value > 20) { 
-    	console.log('green');
-    	setColor(document.body,'green');
+    	setColor(document.body	,'green');
     }
 }   
 
 function setColor(element, color)
 {
-    element.style.background = color;
+    element.style.background = "url('https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2600&q=80')";
 }
