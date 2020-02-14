4d0895323c4ed063d38a2d1d3c62d3ac1a6d6a9e
Elber <elbercastillomesa@gmail.com>
2020-02-13 22:07:20 -0500

    fixing thumb for chrome

--- js/animation.js
+++ js/animation.js
@@ -39,3 +39,30 @@ function setColor(element, value){
 		element.style.backgroundImage = "url(./images/stage_1/bg_1-min.png)";
 	}
 }
+
+
+// EL INPUT
+var elInput3 = document.querySelector('#input3');
+if (elInput3) {
+  var w = parseInt(window.getComputedStyle(elInput3, null).getPropertyValue('width'));
+
+  // LA ETIQUETA
+  var etq = document.querySelector('.etiqueta');
+  if (etq) {
+    // el valor de la etiqueta (el tooltip)
+    etq.innerHTML = elInput3.value;
+
+    // calcula la posición inicial de la etiqueta (el tooltip);
+    var pxls = w / 100;
+
+    etq.style.left = ((elInput3.value * pxls) - 15) + 'px';
+
+    elInput3.addEventListener('input', function() {
+      // cambia el valor de la etiqueta (el tooltip)
+      etq.innerHTML = elInput3.value;
+      // cambia la posición de la etiqueta (el tooltip)
+      etq.style.left = ((elInput3.value * pxls) - 15) + 'px';
+
+    }, false);
+  }
+}
